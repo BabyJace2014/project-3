2a6179c1cb31a2b9e63eaede778c93ecc90f78f4
mpmcauliffe <mcauliffe99@gmail.com>
2018-10-06 07:03:28 -0400

    scss compiler and landing page rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
 import Club from './pages/Club';
 import Create from './pages/Create';
 import Join from './pages/Join';
 import Landing from './pages/Landing';
 import Profile from './pages/Profile';
 
+
+
 class App extends Component {
-  render() {
-    return (
-      <Router>
-    <div>
-      
-      <Switch>
-        <Route exact path="/" component={Club} />
-        <Route exact path="/club" component={Club}/>
-        <Route exact path="/create" component={Create} />
-        <Route exact path="/join" component={Join} />
-        <Route exact path="/landing" component={Landing} />
-        <Route exact path="/profile" component={Profile} />
-        </Switch>
-    </div>
-  </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/club" component={Club}/>
+                        <Route exact path="/create" component={Create} />
+                        <Route exact path="/join" component={Join} />
+                        <Route exact path="/landing" component={Landing} />
+                        <Route exact path="/profile" component={Profile} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
+
 
 export default App;
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React from 'react';
+import { Image, Button } from 'semantic-ui-react';
+
 
 class Landing extends React.Component {
-
-render() {
-    return (
-        <h2>
-            Landing Page
-        </h2>
-        
+    render() {
+        return (
+            <div>
+                
+                <Image src={require(`../../assets/images/tempTitle.png`)} className="temp-title" />
+                
+                <Button primary className="btn btn__landing" >Sign in or sign up with Goodreads</Button> 
+                
+                <ul className="slideshow">
+                    <li>
+                        <Image src={require(`../../assets/images/slideshow/1.png`)} className="cvr-img"  />
+                    </li>
+                    <li>
+                        <Image src={require(`../../assets/images/slideshow/2.png`)} className="cvr-img"  />
+                    </li>
+                    <li>
+                        <Image src={require(`../../assets/images/slideshow/3.png`)} className="cvr-img"  />
+                    </li>
+                    <li>
+                        <Image src={require(`../../assets/images/slideshow/4.png`)} className="cvr-img"  />
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
 
 export default Landing;
+
+
