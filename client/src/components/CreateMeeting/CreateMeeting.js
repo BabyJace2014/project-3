928f5291e54ee4ebc4eab4db2ce584e9b05944ee
DriDoherty <dridoherty@gmail.com>
2018-10-18 18:03:22 -0400

    modify createmeeting to not error out

--- client/src/components/CreateMeeting/CreateMeeting.js
+++ client/src/components/CreateMeeting/CreateMeeting.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Modal, Message, Form, Segment, Button } from 'semantic-ui-react';
 import InlineError from '../InlineError';
 import API from '../../utils/API';
 
 class CreateMeeting extends React.Component {
     state = {
-        date: {},
-        time: {},
-        location: {},
-        book: {},
+        data: { date: "",
+                time: "",
+                host: "",
+                location: "",
+                book: "" },
         errors: {},
         open: false
     }
 
     open = () => this.setState({ open: true });
-    close = () => this.setState({ close: false });
+    close = () => this.setState({ open: false });
 
     componentWillMount = () => {
         this.setState({ user: this.props.user });
@@ -42,17 +42,34 @@ class CreateMeeting extends React.Component {
         if ( Object.keys(errors).length === 0 ) {
 
             let newMeeting = this.state.data;
-
-            newMeeting = {...newMeeting,
-                            location: "",
-                            date: "",
-                            time: "",
-                            book: ""
-                        };
+            this.close();            
+            // newMeeting= {...newMeeting, host: this.state.user.email};
         }
     }
 
-    render() {
+
+    validate = data => {
+        const errors = {};
+
+        if ( !data.meetingname )
+            errors.meetingname = "Please provide a name for this meeting";
+
+        if ( !data.date )
+            errors.date = "Please provide a meeting date";
+
+        if ( !data.time )
+            errors.time = "Please provide a meeting time";
+
+        if ( !data.location )
+            errors.location = "Please provide a meeting location";
+
+        if ( !data.book )
+            errors.book = "Please provide a book for this meeting";
+
+        return errors;
+    }
+
+    render = () => {
         const { data, errors, open } = this.state;
 
         return (
@@ -81,10 +98,11 @@ class CreateMeeting extends React.Component {
                                 name="meetingname"
                                 placeholder='Meeting Name'
                                 autoFocus
-                               // value={data.meetingname}
+                                value={data.meetingname}
                                 onChange={this.onChange} />
                             { errors.meetingname && <InlineError text={errors.meetingname} /> }
                         </Form.Field>
+
                         <Form.Field error={!!errors.date} required>
                             <label htmlFor="date" >Date</label>
                             <input
@@ -92,11 +110,11 @@ class CreateMeeting extends React.Component {
                                 type="text"
                                 name="date"
                                 placeholder='Date'
-                                autoFocus
-                               // value={data.date}
+                                value={data.date}
                                 onChange={this.onChange} />
                             { errors.date && <InlineError text={errors.date} /> }
-                        </Form.Field>
+                            </Form.Field>
+
                             <Form.Field error={!!errors.time} required>
                             <label htmlFor="time" >Time</label>
                             <input
@@ -105,22 +123,23 @@ class CreateMeeting extends React.Component {
                                 name="time"
                                 placeholder='Time'
                                 autoFocus
-                               // value={data.time}
+                                value={data.time}
                                 onChange={this.onChange} />
                             { errors.time && <InlineError text={errors.time} /> }
                             </Form.Field>
-                            <Form.Field error={!!errors.date} required>
+
+                            <Form.Field error={!!errors.location} required>
                             <label htmlFor="location" >Location</label>
                             <input
                                 fluid='true'
                                 type="text"
                                 name="location"
                                 placeholder='Location'
-                                autoFocus
-                               // value={data.location}
+                                value={data.location}
                                 onChange={this.onChange} />
-                            { errors.date && <InlineError text={errors.location} /> }
+                            { errors.location && <InlineError text={errors.location} /> }
                             </Form.Field>
+
                             <Form.Field error={!!errors.book} required>
                             <label htmlFor="book" >Book</label>
                             <input
@@ -128,10 +147,9 @@ class CreateMeeting extends React.Component {
                                 type="text"
                                 name="book"
                                 placeholder='Book'
-                                autoFocus
-                               // value={data.book}
+                                value={data.book}
                                 onChange={this.onChange} />
-                            { errors.date && <InlineError text={errors.date} /> }
+                            { errors.book && <InlineError text={errors.book} /> }
                             </Form.Field>
                     </Segment>
                     </Form>
